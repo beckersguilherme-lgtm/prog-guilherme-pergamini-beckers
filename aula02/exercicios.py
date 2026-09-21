aa91369b8cdf65dfd14c2d283c67a92e5ccfd23b
beckersguilherme-lgtm <beckers.guilherme@escola.pr.gov.br>
2026-09-21 12:23:38 +0000

    receba

--- aula02/exercicios.py
+++ aula02/exercicios.py
@@ -7,27 +7,48 @@ Escreva sua solucao no lugar do 'pass'.
 
 def remove_negativos(lista):
     """Devolve uma lista nova so com os numeros que nao sao negativos."""
-    pass
+
+    return [x for x in lista if x >= 0]
 
 
 def inverte(lista):
     """Devolve uma lista nova na ordem contraria.
     Sem usar reverse() e sem usar [::-1]."""
-    pass
+
+    nova_lista = []
+    for i in range(len(lista) - 1, -1, -1):
+        nova_lista.append(lista[i])
+    return nova_lista
 
 
 def busca_binaria(lista, alvo):
     """Recebe uma lista JA ORDENADA. Devolve a posicao do alvo, ou -1."""
-    pass
+    passdef remove_negativos(lista):
+
+def busca_binaria(lista, alvo):
+    """Recebe uma lista JÁ ORDENADA. Devolve a posição do alvo, ou -1."""
+    inicio = 0
+    fim = len(lista) - 1
+
+    while inicio <= fim:
+        meio = (inicio + fim) // 2
+        if lista[meio] == alvo:
+            return meio
+        elif lista[meio] < alvo:
+            inicio = meio + 1
+        else:
+            fim = meio - 1
+
+    return -1   
+
 
 
 def intercala(lista_a, lista_b):
     """Devolve uma lista nova alternando os elementos das duas.
     As duas listas tem o mesmo tamanho."""
-    pass
 
-
-def remove_repetidos(lista):
-    """(Desafio) Devolve uma lista nova sem repetidos,
-    mantendo a ordem da primeira aparicao."""
-    pass
+    iresultado = []
+    for i in range(len(lista_a)):
+        resultado.append(lista_a[i])
+        resultado.append(lista_b[i])
+    return resultado
